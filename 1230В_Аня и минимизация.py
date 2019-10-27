c0f0b38eb1e885c249c192ee6bb4f76d31371e9c
Roman Usyk <romanysik@gmail.com>
2019-10-27 22:39:28 +0200

    Refactor 1230B

--- 1230В_Аня и минимизация.py
+++ 1230В_Аня и минимизация.py
@@ -1,25 +1,14 @@
-n,k = map(int,input().split())
+n, k = map(int, input().split())
 s = input()
 s = list(s)
-if n == 1 and k > 0:
-    print(int(0))
 
-elif k == 0:
-    print(''.join(map(str,s)))
-    
-else:
-    c = 0
-    if s[0] > '1':
-        s[0] = 1
-        c = c + 1
-    
-    for i in range(1,len(s)):
-        if c == k:
-            break
-        if s[i] == '0':
-            continue
-        else:
-            s[i] = 0
-            c = c + 1
+for i in range(n):
+	if k == 0:
+		break
+	replacement = '1' if i == 0 and n > 1 else '0'
+	if s[i] == replacement:
+		continue
+	s[i] = replacement
+	k -= 1
 
-    print(''.join(map(str,s)))
+print(''.join(s))
