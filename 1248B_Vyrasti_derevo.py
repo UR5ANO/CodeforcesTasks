712e44b02e152592a1fffc34f3441dc41c1e2d9c
Vova <goodwaldemar@ukr.net>
2019-11-10 12:21:27 +0200

    Edit 1248B

--- 1248B_Vyrasti_derevo.py
+++ 1248B_Vyrasti_derevo.py
@@ -1,2 +1,16 @@
+n = int(input())
+s = map(int,input().split())
+s = list(s)
+s.sort()
+ 
 import math
-print(math.gcd(2,5))
+minim = 0
+for i in range(math.floor(len(s)/2)):
+        minim = minim + s[i]
+ 
+s.reverse()
+maxim = 0
+for i in range(math.ceil(len(s)/2)):
+        maxim = maxim + s[i]
+ 
+print(minim**2 + maxim**2)
