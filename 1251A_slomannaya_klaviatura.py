79887b3bac1d60f5de9167398354c18704c55a84
Vova <goodwaldemar@ukr.net>
2019-11-21 19:35:10 +0200

    change 1251A

--- 1251A_slomannaya_klaviatura.py
+++ 1251A_slomannaya_klaviatura.py
@@ -7,13 +7,13 @@ for _ in range(n):
     counter = 0
 
     s = s + '0'
-    for l in s:
-        if l == current_letter:
+    for k in s:
+        if k == current_letter:
             counter += 1
         else:
             if counter % 2 != 0:
                 good_letters.add(current_letter)
-            current_letter = l
+            current_letter = k
             counter = 1
-
+        print(good_letters)
     print(''.join(sorted(good_letters)))
